2241d1981b75f0ad7cfed2abec8549e969232d29
jinyuan sun <33344948+JinyuanSun@users.noreply.github.com>
2021-11-19 16:35:11 +0800

    Update foldx.py

--- utlis/foldx.py
+++ utlis/foldx.py
@@ -5,6 +5,7 @@
 import os
 import pandas as pd
 
+
 class FoldX:
     def __init__(self, pdbName, path2foldx, numThreads, cutOff):
         self.pdbname = pdbName
@@ -13,14 +14,13 @@ class FoldX:
         self.cutoff = cutOff
         self.result = []
 
-
     def repairPDB(self):
         cmd = self.path + 'foldx --command=RepairPDB --pdb=' + self.pdbname
         pdbname = self.pdbname
         # print(cmd)
-        print('=='*20)
+        print('==' * 20)
         print('Stage 1: Repair PDB file using FoldX')
-        print('=='*20)
+        print('==' * 20)
         print('[INFO]: Running FoldX RepairPDB.')
 
         FoldX_out = os.popen(cmd).read()
@@ -32,7 +32,7 @@ class FoldX:
         return pdbname.replace(".pdb", '_Repair.pdb')
 
     def calScore(self, wild, resNum, mutation, pdbfile, jobID):
-        fxout_name = jobID + "Dif_" + pdbfile.replace(".pdb", '.fxout')
+        fxout_name = jobID + "/Dif_" + pdbfile.replace(".pdb", '.fxout')
         # print(fxout_name)
         df = pd.read_table(fxout_name, sep='\t', skiprows=8)
         score = round(df['total energy'].mean(), 4)
@@ -40,17 +40,22 @@ class FoldX:
         self.result.append(["_".join([wild, str(resNum), mutation]), score, sd])
         return ["_".join([wild, str(resNum), mutation]), score, sd]
 
-    def runOneJob(self, pdbfile, wild, chain, mutation, resNum, jobID):
-        os.mkdir(jobID)
-        os.chdir(jobID)
+    def runOneJob(self, varlist: list):
+        pdbfile, wild, chain, mutation, resNum, jobID = varlist
+        # mutation_name = "_".join([wild, str(resNum), mutation])
+        try:
+            os.mkdir(jobID)
+            os.chdir(jobID)
+        except FileExistsError:
+            os.chdir(jobID)
+
         with open('individual_list.txt', 'w+') as indFile:
             indFile.write(wild + chain + str(resNum) + mutation + ";")
             indFile.close()
         cmd1 = "cp ../../" + pdbfile + " ./"
         os.popen(cmd1)
-        cmd2 = self.path + "foldx --command=BuildModel --numberOfRuns=10 --mutant-file=individual_list.txt --pdb=" + pdbfile
+        cmd2 = self.path + "foldx --command=BuildModel --numberOfRuns=10 --mutant-file=individual_list.txt --pdb=" + \
+               pdbfile + " 1>/dev/null"
         os.system(cmd2)
-        os.chdir("../../../")
-        return self.calScore(self, pdbfile) #need self?
-
-
+        os.chdir("../../")
+        # return self.calScore(self, pdbfile)  # need self?
