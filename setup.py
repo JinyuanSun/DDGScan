4b63ed24e0fe7d11f14c6510f1bd2fd51d39308e
jysun <1650949260@qq.com>
2022-03-26 04:22:25 +0800

    update

--- setup.py
+++ setup.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-install_requires = [
-    'mdtraj',
-    'pandas',
-    'numpy',
-    'joblib',
-    'sklearn'
-    ]
+
 
 setup(
     name="GRAPE",
@@ -17,5 +11,11 @@ setup(
     description="A powerful package for GRAPE",
     url="https://github.com/JinyuanSun/DDGScan",
     packages=find_packages(),
-    scripts=['grape-fast.py', 'gluster.py']
+    scripts=['grape-fast.py', 'gluster.py'],
+    install_requires=['mdtraj',
+                      'pandas',
+                      'numpy',
+                      'joblib',
+                      'sklearn'
+    ]
 )
