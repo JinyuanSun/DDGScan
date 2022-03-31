fa3800feb897faf223e0c5f13cc5efc811b2be85
jysun <1650949260@qq.com>
2022-04-01 03:51:17 +0800

    update

--- utlis/judge.py
+++ utlis/judge.py
@@ -1,8 +1,57 @@
 #!/usr/bin/env python
-from .io import Protein
+class Protein:
+    def __init__(self, pdbname, chain):
+        self.pdbname = pdbname
+        self.chain = chain
+        self.seq = ''
+        self.resNumList = []
 
+
+    def _3_2_1(self, x):
+        d = {
+            "CYS": "C",
+            "ASP": "D",
+            "SER": "S",
+            "GLN": "Q",
+            "LYS": "K",
+            "ILE": "I",
+            "PRO": "P",
+            "THR": "T",
+            "PHE": "F",
+            "ASN": "N",
+            "GLY": "G",
+            "HIS": "H",
+            "LEU": "L",
+            "ARG": "R",
+            "TRP": "W",
+            "ALA": "A",
+            "VAL": "V",
+            "GLU": "E",
+            "TYR": "Y",
+            "MET": "M",
+        }
+        y = d.get(x)
+        assert y, f"{x} dose not belong to 20 canonical animo acids!"
+        return y
+
+    def pdb2seq(self):
+        with open(self.pdbname) as pdbfile:
+            for line in pdbfile:
+                if "ATOM" == line[0:6].replace(" ", ""):
+                    if self.chain == line[21].replace(" ", ""):
+                        if line[12:16].replace(" ", "") == "CA":
+                            if line[16] == "B":
+                                continue
+                            else:
+                                self.seq += self._3_2_1(line[17:20].replace(" ", ""))
+                                self.resNumList.append(
+                                    int(line[22:26].replace(" ", ""))
+                                )
+        pdbfile.close()
+        return self.seq, self.resNumList
 
 def judge(userSeq, seq, resNumList):
+    # print(resNumList, resNumList)
     listLen = int(resNumList[-1]) - int(resNumList[0]) + 1
     resLen = len(seq)
     # print(listLen, resLen)
@@ -13,8 +62,10 @@ def judge(userSeq, seq, resNumList):
 
 
 def main(pdb, chain, userSeq):
-    userPdb = Protein(pdb, chain)
-    return judge(userSeq, userPdb.seq, userPdb.resNumList)
+    seq,  resNumList = Protein(pdb, chain).pdb2seq()
+    judge_result = judge(userSeq, seq, resNumList)
+    if judge_result != 0:
+        print("Chain break detected! Transfer to AlphaFold.")
 
 
 if __name__ == "__main__":
@@ -23,8 +74,7 @@ if __name__ == "__main__":
     pdb = sys.argv[1]
     chain = sys.argv[2]
     userSeq = sys.argv[3]
-    userPdb = Protein(pdb, chain)
-    judge_result = judge(userSeq, userPdb.seq, userPdb.resNumList)
-    # print(userSeq, userPdb.seq, userPdb.resNumList)
+    seq,  resNumList = Protein(pdb, chain).pdb2seq()
+    judge_result = judge(userSeq, seq, resNumList)
     if judge_result != 0:
         print("Chain break detected! Transfer to AlphaFold.")
